badbfad88e62ade21b5e7a94aee66950dd736259
NoahCCW <lahaienoah@gmail.com>
2026-01-12 10:19:50 -0500

    added stuff

---
+++ public/js/evil.js
@@ -0,0 +1,5 @@
+function activateEvil() {
+
+alert('Evil');
+
+}
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,9 @@ function message() {
 
 let name = prompt("enter your name");
 alert('Greetings, ' + name + '.');
-
+if (name ==  "evil man") {
+alert('Entering evil land');
+window.open("evil.html");
+};
 
 };
